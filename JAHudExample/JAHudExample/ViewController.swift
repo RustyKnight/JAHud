43fa49cffd467975b6c933ff56b55ef08de7667b
Shane Whitehead <obishiro@gmail.com>
2018-09-09 09:10:54 +1000

    Expand the example to include examples of both iOS and material indicators and colorful examples

--- JAHudExample/JAHudExample/ViewController.swift
+++ JAHudExample/JAHudExample/ViewController.swift
@@ -16,33 +16,92 @@ class ViewController: UIViewController {
 	}
 	
 	var progress: Progress = Progress(totalUnitCount: 100)
+	
+	func makeItShow(_ config: Hud.Configuration, fail: Bool) {
+		Hud.presentProgress(on: self,
+												progress: progress,
+												title: "All your work are belong to us",
+												text: "Please wait...",
+												configuration: config) {
+			DispatchQueue.global(qos: .userInitiated).async {
+				while self.progress.fractionCompleted < 1.0 {
+//					Thread.sleep(forTimeInterval: Double.random(in: 1.0...5.0))
+//					let amount = Int.random(in: 5...10)
+					Thread.sleep(forTimeInterval: 1.0)
+					let amount = 10
+					let value = min(100, self.progress.completedUnitCount + Int64(amount))
+					self.progress.completedUnitCount += value
+				}
+				Thread.sleep(forTimeInterval: 1.0)
+				DispatchQueue.main.async {
+					if fail {
+						Hud.presentFailure(on: self) {
+							Hud.dismiss(from: self)
+						}
+					} else {
+						Hud.presentSuccess(on: self) {
+							self.performSegue(withIdentifier: "After", sender: self)
+						}
+					}
+				}
+			}
+		}
+	}
 
-	@IBAction func makeItShow(_ sender: Any) {
+	@IBAction func makeItMaterial(_ sender: Any) {
+		progress.completedUnitCount = 0
+		var config = Hud.Configuration()
+		config.progress.strokeWidth = 3.0
+		config.waitIndicatorStyle = .material
+		
+		makeItShow(config, fail: true)
+	}
+
+	@IBAction func makeItiOS(_ sender: Any) {
+		progress.completedUnitCount = 0
+		var config = Hud.Configuration()
+		config.progress.strokeWidth = 3.0
+
+		makeItShow(config, fail: false)
+	}
+
+	@IBAction func makeItMaterialWithColor(_ sender: Any) {
 		progress.completedUnitCount = 0
 		var config = Hud.Configuration()
 		config.progress.strokeWidth = 3.0
 		config.state.fillStyle = .filled
 		config.waitIndicatorStyle = .material
 		
-		Hud.presentProgress(on: self, progress: progress, title: "All your work are belong to us", text: "Please wait...", configuration: config) {
-			DispatchQueue.global(qos: .userInitiated).async {
-				while self.progress.fractionCompleted < 1.0 {
-					Thread.sleep(forTimeInterval: Double.random(in: 1.0...5.0))
-					let amount = Int.random(in: 5...10)
-					let value = min(100, self.progress.completedUnitCount + Int64(amount))
-					self.progress.completedUnitCount += value
-				}
-				Thread.sleep(forTimeInterval: 1.0)
-				DispatchQueue.main.async {
-					Hud.presentSuccess(on: self) {
-						//Hud.dismiss(from: self)
-						self.performSegue(withIdentifier: "After", sender: self)
-					}
-				}
-			}
-		}
+		config.contentBackgroundColor = UIColor.blue.withAlphaComponent(0.5)
+		config.textColor = UIColor.red
+		config.titleColor = UIColor.white
+		config.waitIndicatorColor = UIColor.magenta
+		config.progress.strokeColor = UIColor.purple
+		config.state.fillStyle = .filled
+		config.state.failColor = .orange
+		config.state.successColor = .green
+		
+		makeItShow(config, fail: true)
 	}
 	
+	@IBAction func makeItiOSWithColor(_ sender: Any) {
+		progress.completedUnitCount = 0
+		var config = Hud.Configuration()
+		config.progress.strokeWidth = 3.0
+		config.state.fillStyle = .filled
+
+		config.contentBackgroundColor = UIColor.blue.withAlphaComponent(0.5)
+		config.textColor = UIColor.red
+		config.titleColor = UIColor.white
+		config.waitIndicatorColor = UIColor.magenta
+		config.progress.strokeColor = UIColor.purple
+		config.state.fillStyle = .filled
+		config.state.failColor = .orange
+		config.state.successColor = .green
+
+		makeItShow(config, fail: false)
+	}
+
 	override func viewDidDisappear(_ animated: Bool) {
 		super.viewDidDisappear(animated)
 		Hud.dismiss(from: self)
