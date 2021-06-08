29fd19965c841769e1acef558129a85e665fc67b
Shane Whitehead <shanew@swann.com>
2021-06-08 20:28:15 +1000

    Support for xcframeworks

--- update.sh
+++ update.sh
@@ -1,2 +1,3 @@
 xcodebuild -version
-time carthage update --platform iOS --configuration Debug
+#time carthage update --platform iOS --configuration Debug
+time carthage update  --use-xcframeworks --platform iOS --configuration Debug --cache-builds $1
