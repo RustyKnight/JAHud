60ce09d2dcac3f53ecd4c1b50ac44af8ac2d6e8c
Shane Whitehead <shanew@swann.com>
2021-06-24 11:12:17 +1000

    Remove support for xcodeframe[S]

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
 xcodebuild -version
 #time carthage update --platform iOS --configuration Debug
-time carthage update  --use-xcframeworks --platform iOS --configuration Debug --cache-builds $1
+#time carthage update  --use-xcframeworks --platform iOS --configuration Debug --cache-builds $1
+time swncarthage update --skip-simulators --platform iOS --configuration Debug --cache-builds $1
