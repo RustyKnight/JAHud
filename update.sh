438236ffd29826f399d4ac1f6606f84ac7d1ed65
Shane Whitehead <obishiro@gmail.com>
2024-01-16 14:40:25 +1100

    Remove Carthage support Add support for SPM

--- update.sh
+++
@@ -1,4 +0,0 @@
-xcodebuild -version
-#time carthage update --platform iOS --configuration Debug
-#time carthage update  --use-xcframeworks --platform iOS --configuration Debug --cache-builds $1
-time swncarthage update --skip-simulators --platform iOS --configuration Debug --cache-builds $1
